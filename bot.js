b43b0e40c854232dd580870e939bd54c4ba8251a
Kyle <39460375+xkyrux@users.noreply.github.com>
2020-03-11 09:50:05 -0700

    Update bot.js
    Added the line of code that makes your bot listen to port 3000. This is needed if you use uptime robot to keep Glitch running

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@
  * Version 1.5.10
  * Robert Borghese
  ******************************************************/
-
+require("http").createServer(async (req,res) => { res.statusCode = 200; res.write("ok"); res.end(); }).listen(3000, () => console.log("Now listening on port 3000"));
 const DBM = {};
 DBM.version = "1.5.10";
 
